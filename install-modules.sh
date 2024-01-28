11fd9b8db6da26146e07f6ab3bde6b9919c10f97
phoenixyork166 <127727126+phoenixyork166@users.noreply.github.com>
2024-01-28 11:13:03 +0800

    Add files via upload

---
+++ install-modules.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+modules=(
+    'all_packages'
+    'colorama'
+    'docopt'
+    'freeze'
+    'numpy'
+    'pandas'
+    'pytest'
+    'pytest-cov'
+    'selenium'
+    'retry'
+    'metplotlib'
+    'scikit-learn'
+    'jedi'
+    'scipy'
+    'scapy'
+    'idlex'
+)
+
+for module in ${modules[@]};
+    do
+        echo "Installing Python module: ${module}";
+        installMod=$(python -m pip install ${module} --user IGS);
+        if [[ ${?} -eq 0 ]];
+        then
+            echo "======================================";
+            echo "";
+            echo "Succeeded in installing Python3-${module}";
+            echo "";
+            echo "======================================";
+
+        else
+            echo "======================================";
+            echo "";
+            echo "Failed to install ${module}";
+            echo "";
+            echo "======================================";
+        fi
+    done
