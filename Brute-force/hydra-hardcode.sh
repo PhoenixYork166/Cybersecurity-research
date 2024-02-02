d6f3ef0ad1bcbcd3405a2bf4ca07ab550b2d722d
phoenixyork166 <berlinlee2017@gmail.com>
2024-02-02 23:11:53 +0800

    adding Brtue-force namelists

--- Brute-force/hydra-hardcode.sh
+++ Brute-force/hydra-hardcode.sh
@@ -4,10 +4,13 @@
 nameList='/usr/share/wordlists/metasploit/namelist.txt';
 
 # -P
-wordList='/usr/share/wordlists/rockyou.txt';
+#wordList='/usr/share/wordlists/rockyou.txt';
+#wordList='/usr/share/brutex/wordlists/namelist.txt';
+#wordList='/usr/share/dnsrecon/namelist.txt';
+wordList='/usr/share/sniper/plugins/BruteX/wordlists/namelist.txt';
 
 # sudo password
-#read -p "Enter sudo password: " sudo_passwd;
+#read -p "Enter sudo password: " -r -s sudo_passwd;
 
 # Target IP
 #read -p "Enter target IP [192.168.2.65]: " target;
@@ -15,11 +18,11 @@ target='192.168.2.65';
 
 # Port
 #read -p "Enter ${target}:PORT [8080/8081/8082]: " port;
-port=8082;
+port='8082';
 
 # -t 
 #read -p "Enter thread [4-16]: " thread;
-thread=16;
+thread='16';
 
 # Report path
 user=$(whoami);
--- Brute-force/hydra.sh
+++ Brute-force/hydra.sh
@@ -7,9 +7,22 @@ nameList='/usr/share/wordlists/metasploit/namelist.txt';
 wordList='/usr/share/wordlists/rockyou.txt';
 
 # sudo password
-read -p "Enter sudo password: " sudo_passwd;
+read -p "Enter sudo password: " -r -s sudo_passwd;
 # Target IP
 read -p "Enter target IP [192.168.2.65]: " target;
+# if [[ $target =~ ^[+-]?[0-9]+$ ]];
+# then
+#     echo "${target} is an INT";
+# elif [[ $target =~ ^[+-]?[0-9]+\.$ ]];
+# then
+#     echo "Input is a STRING";
+# elif [[ $target =~ ^[+-]?[0-9]+\.?[0-9]*$ ]];
+# then
+#     echo "${target} is a Float";
+# else
+#     echo "${target} is a STRING";
+# fi
+
 # Port
 read -p "Enter ${target}:PORT [8080/8081/8082]: " port;
 
