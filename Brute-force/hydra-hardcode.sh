0dd21c25e925f4a39dbe1975bd5c9c12dc11828f
phoenixyork166 <berlinlee2017@gmail.com>
2024-02-05 12:14:27 +0800

    buffer-overflow

--- Brute-force/hydra-hardcode.sh
+++ Brute-force/hydra-hardcode.sh
@@ -16,12 +16,12 @@ wordList='/usr/share/wordlists/rockyou.txt';
 #read -p "Enter sudo password: " -r -s sudo_passwd;
 
 # Target IP
-#read -p "Enter target IP [192.168.2.65]: " target;
-target='192.168.2.65';
+read -p "Enter target IP [192.168.2.65]: " target;
+#target='192.168.2.65';
 
 # Port
-#read -p "Enter ${target}:PORT [8080/8081/8082]: " port;
-port='8082';
+read -p "Enter ${target}:PORT [8080/8081/8082/20145]: " port;
+#port='8082';
 
 # -t 
 #read -p "Enter thread [4-16]: " thread;
