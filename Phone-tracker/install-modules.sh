1e20b5451eb61870ba6390202c6cdccffc059c7d
phoenixyork166 <berlinlee2017@gmail.com>
2024-02-08 17:19:34 +0800

    install-modules.sh

--- Phone-tracker/install-modules.sh
+++ Phone-tracker/install-modules.sh
@@ -13,8 +13,26 @@ modules=(
 # Windows
 for module in ${modules[@]};
     do
-        echo "Installing Python module: ${module}";
+        echo "Installing Windows Python module: ${module}";
         installMod=$(python -m pip install ${module} --user IGS);
+        if [[ ${?} -eq 0 ]];
+        then
+            echo "======================================";
+            echo "";
+            echo "Succeeded in installing Python3-${module}";
+            echo "";
+            echo "======================================";
+
+        else
+            echo "======================================";
+            echo "";
+            echo "Failed to install ${module}";
+            echo "";
+            echo "======================================";
+        fi
+
+        echo "Installing Linux Python module: ${module}";
+        installLinuxMod=$(apt install -y python3-${module});
         if [[ ${?} -eq 0 ]];
         then
             echo "======================================";
