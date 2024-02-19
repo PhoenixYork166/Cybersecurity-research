2586b830520b8a992c57d34ce7d63c9c4a580e34
phoenixyork166 <berlinlee2017@gmail.com>
2024-02-19 20:00:22 +0800

    mac-changer.py

--- MAC-changer/mac-changer.py
+++ MAC-changer/mac-changer.py
@@ -3,7 +3,12 @@
 # Bypass Filters
 
 # #!/usr/bin/env python3
+# Use terminal ;)
 import subprocess
+# Styling Python ;)
+from colorama import Fore, Back, Style
+# Timing ;)
+import time
 # import optparse
 
 # parser = optparse.OptionParser()
@@ -15,6 +20,10 @@ import subprocess
 ifconfig = f'ifconfig'
 NIC = input(f'Enter NIC name to change MAC addr [eth0]: ')
 new_MAC = input(f'Enter new MAC addr for {NIC} [00:5c:56:aa:bb:cc]: ')
+
+# Get current local time as a struct time OBJ
+current_time = time.localtime()
+formatted_time = time.strftime('%Y-%m-%d-%H:%M:%S', current_time)
 
 def changeMAC():
     do_ifconfig = subprocess.Popen(ifconfig, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -47,15 +56,16 @@ def changeMAC():
 
     if do_change_MAC.returncode == 0:
         print(f'\n')
-        print(f'{do_change_MAC_out}')
+        print(f'{do_change_MAC_out}\nat {formatted_time}')
         print(f'\n')
         print(f'\n')
     else:
         print(f'\n')
-        print(f'{do_change_MAC_err}')
+        print(f'{do_change_MAC_err}\nat {formatted_time}')
         print(f'\n')
         print(f'\n')
 
+    # Bring NIC back online
     NIC_on = f'{ifconfig} {NIC} up'
     do_NIC_on = subprocess.Popen(NIC_on, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     do_NIC_on_out, do_NIC_on_err = do_NIC_on.communicate()
@@ -68,6 +78,18 @@ def changeMAC():
         print(f'\n')
         print(f'{do_NIC_on_err}')
         print(f'Failed to turn ON {NIC}')
+        
+    # Force restart networking.service
+    restartNetwork = f'/etc/init.d/networking restart'
+    do_restartNetwork = subprocess.Popen(restartNetwork, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    do_restartNetwork_out, do_restartNetwork_err = do_restartNetwork.communicate()
+    if do_restartNetwork.returncode == 0:
+        print(f'\n')
+        print(Fore.WHITE + f'{do_restartNetwork_out}')
+        print(f'\n')
+        print(Fore.YELLOW + f'Succeeded in restarting network.service ;)\nat {formatted_time}')
+        print(f'\n')
+        print(f'\n')        
 
 # if this script = main script => Execute
 if __name__ == '__main__':
